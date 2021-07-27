8dab33b0e55166a421a4c9917a7edd125ae01257
samuelsells123 <samuelsells123@gmail.com>
2021-07-27 12:28:07 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  userData: object = {
+  loginUser: object = {
     "userId" : 3,
     "firstName" : "Samuel",
     "lastName" :  "Sells",
